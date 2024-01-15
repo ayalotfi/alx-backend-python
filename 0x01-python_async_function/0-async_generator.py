2caa8cbcb1e4fa2f2df87805652e843bca23d42d
ayalotfi <ayalotfi59@gmail.com>
2024-01-15 13:00:17 -0800

    py

--- 0x01-python_async_function/0-async_generator.py
+++ 0x01-python_async_function/0-async_generator.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
-"""
-This module provides an asynchronous generator function that yields a
-random float between 0 and 10 after a one second delay for a total of
-10 iterations.
-"""
+'''Task 0's module.
+'''
 import asyncio
 import random
-from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """
-    Asynchronous generator function that yields a random float between 0 and 10
-    after a one second delay for a total of 10 iterations.
-
-    Returns:
-        Generator: Asynchronous generator object that can be used in an
-        awaitable context.
-    """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.random() * 10
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-"""
-This module provides an asynchronous function that measures the runtime
-of running 4 instances of the async_comprehension function from the
-'1-async_comprehension' module
-"""
+'''Task 2's module.
+'''
 import asyncio
 import time
-from importlib import import_module as using
-
-async_comprehension = using('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> float:
-    """
-    Asynchronous function that measures and returns the runtime of
-    running 4 instances of the async_comprehension function.
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-    Returns:
-        float: The runtime of the async_comprehension function in seconds.
-    """
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''Computes the average runtime of wait_n.
+    '''
     start_time = time.time()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.time() - start_time
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
